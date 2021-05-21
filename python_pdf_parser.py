65ea8719aca4952d8b5e43ed97f5f3e1d81bd486
Linsey Stokes <linseystokes17@gmail.com>
2021-05-21 08:51:38 -0500

    trying to fix errors in the text

--- python_pdf_parser.py
+++ python_pdf_parser.py
@@ -6,28 +6,35 @@ import pdfplumber
 import re
 
 
-pdf = pdfplumber.open('braiding_sweetgrass.pdf')
-pages = pdf.pages[9:458]
+pdf = pdfplumber.open('ch05.pdf')
+pages = pdf.pages[2:]
 comment_words = ''
 stopwords = set(STOPWORDS)
 words = []
 for page in pages:
-    val = str(page.extract_text())
-    word = re.findall(r"[\w']+", val)
-    for w in word:
-        w = w.lower()
-        if len(w) <=2:
-            w=""
-    comment_words+= " ".join(word)+" "
-wordcloud = WordCloud(width = 800, height = 800,
-                background_color ='white',
-                stopwords = stopwords,
-                min_font_size = 10).generate(comment_words)
+    print(page.extract_text())
+#     val = str(page.extract_text())
+#     word = re.findall(r"[\w']+", val)
+    
+#     for w in word:# filter for numbers, single letters, chapter. Make everything lowercase
+#         if w.isupper():
+#             # print(w)
+#             word.remove(w)
+#         if w.isdigit() and len(w)<4:
+#             word.remove(w)
+#         #print(w)
+#     comment_words+= " ".join(word)+" "
 
-# plot the WordCloud image                       
-plt.figure(figsize = (8, 8), facecolor = None)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.tight_layout(pad = 0)
+# print(comment_words)
+# wordcloud = WordCloud(width = 800, height = 800,
+#                 background_color ='white',
+#                 stopwords = stopwords,
+#                 min_font_size = 10).generate(comment_words)
+
+# # plot the WordCloud image                       
+# plt.figure(figsize = (8, 8), facecolor = None)
+# plt.imshow(wordcloud)
+# plt.axis("off")
+# plt.tight_layout(pad = 0)
   
-plt.show()
+# plt.show()
